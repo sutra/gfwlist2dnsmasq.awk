a103b735dc2301a0ebac83876bd9070b5353d6be
Joe Zhou <zhoushuqun@gmail.com>
2017-01-28 21:40:31 +0800

    Rename the OpenWrt script.

--- gfwlist2dnsmasq-openwrt.sh
+++ gfwlist2dnsmasq-openwrt.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-# gfwlist2dnsmasq on Openwrt
-# wget is more common in Openwrt
-# need install 'coreutils-base64' first
-# openwrt dnsmasq can only restart by /etc/init.d/dnsmasq restart
-# set your custom here
+# gfwlist2dnsmasq on OpenWrt
+# wget is more common on OpenWrt
+# 'coreutils-base64' is required
+# OpenWrt dnsmasq can only be restarted by /etc/init.d/dnsmasq restart
 HOST='127.0.0.1'
 PORT=5353
 IPSET='gfwlist'
